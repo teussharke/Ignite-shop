babe836bfbc1c183008bc29f55323305f8c45cef
Mateus Oliveira <109149179+teussharke@users.noreply.github.com>
2023-04-25 14:26:51 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
-import { styled } from '../styles'
+import { HomeCointainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeCointainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+          <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+          <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeCointainer>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,9 +1,15 @@
 import { styled } from "..";
 
 export const HomeCointainer = styled('main', {
-
+  display: 'flex',
+  gap: '3rem',
+  backgroundColor: 'blue',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  marginLeft: 'auto',
+  minHeight: 656,
 })
 
-export const Product = styled('div', {
+export const Product = styled('a', {
 
 })
