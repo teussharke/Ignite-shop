3ae6a65c1f154f210a3d950630e9c01ac5eda3cb
Mateus Oliveira <109149179+teussharke@users.noreply.github.com>
2023-05-08 16:00:19 -0300

    att

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,6 +13,7 @@ export const {
   theme: {
     colors: {
       white: '#FFF',
+
       gray900: '#121214',
       gray800: '#202024',
       gray300: '#c4c4cc',
@@ -21,12 +22,12 @@ export const {
       green500: '#00875f',
       green300: '#00b37e',
     },
+
     fontSizes: {
       md: '1.125rem',
-      lg: '1.125rem',
+      lg: '1.25rem',
       xl: '1.5rem',
       '2xl': '2rem',
-    },
+    }
   }
 })
-
